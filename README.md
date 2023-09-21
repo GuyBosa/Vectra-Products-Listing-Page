676f67b064feb077db780458734765242b332e54
Guy Bosa <guybosa0@gmail.com>
2023-09-21 06:02:17 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Vectra Responsive Product Listing Page
+
+>Made with bootstrap4
+> and Vue/Vuex/Vue-router.
+
+## Development/Producation Setup
+
+``` bash
+# Open the project folder
+cd product-listing-page && npm install
+
+# FOR DEV: If you have **vue/cli 3** then through **vue ui** you can import the file and serve it. else 
+npm run serve
+
+# FOR BUILD: 
+npm run build
+```
---
+++ src/Components/ProductsPage/slider.vue
@@ -0,0 +1,45 @@
+<template>
+    <div @click="getValue('sli')">
+      <vue-slider ref="sli" v-model="value" v-bind="options"></vue-slider>
+      <span>R{{ value.join(' ~ R') }}</span>
+    </div>
+</template>
+
+<script>
+import vueSlider from 'vue-slider-component'
+
+export default {
+  name: 'slider',
+  components: {
+    vueSlider
+  },
+  data() {
+    return {
+      value: [
+        36,
+        756
+      ],
+      options: {
+      width: "auto",
+      height: 4,
+      dotSize: 10,
+      min: 36,
+      max: 756,
+      show: true,
+      useKeyboard: true,
+      tooltip: "hover",
+      formatter: (v) => `R${v}`,
+      mergeFormatter: (v1, v2) => `R${v1} ~ R${v2}`,
+      processStyle: {
+        "backgroundColor": "#999"
+      }}
+    }
+  },
+  methods: {
+    getValue(name) {
+      let slider = this.$refs[name]
+      this.$emit('clicked', slider.getValue())
+    }
+  }
+}
+</script>
